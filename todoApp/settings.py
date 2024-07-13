173ae86a4b0dc6fbdcf5c5681a1cdebb29b35d48
abhi5981 <abhi5981@gmail.com>
2024-07-14 00:27:35 +0530

    Updating settings for template  UI

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -56,7 +56,8 @@ ROOT_URLCONF = 'todoApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
+        # 'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
